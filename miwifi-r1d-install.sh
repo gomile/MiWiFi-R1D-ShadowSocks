3b0d48b0807799839ddd895cd0392b249726fd4c
kiss4437 <16162839@qq.com>
2018-04-12 20:24:31 +0800

    Update miwifi-r1d-install.sh

--- miwifi-r1d-install.sh
+++ miwifi-r1d-install.sh
@@ -30,12 +30,12 @@ if [ -f "/etc/shadowsocks.json" ]; then
 fi
 cat > /etc/shadowsocks.json<<-EOF
 {
-  "server": ${serverip},
-  "server_port": ${serverport},
-  "local_port": 1081,
-  "password": ${shadowsockspwd},
-  "timeout": 600,
-  "method": ${method}
+  "server" : "${serverip}",
+  "server_port" : "${serverport}",
+  "local_port" : "1081",
+  "password" : "${shadowsockspwd}",
+  "timeout" : "600",
+  "method" : "${method}"
 }
 EOF
 
